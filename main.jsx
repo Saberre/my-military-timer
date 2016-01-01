f83e8735ec218dc910792e9b964532fedafc58d4
Ki-baek Lee <saberre@gmail.com>
2016-01-01 22:31:30 +0900

    Hardcoded target timestamp

--- main.jsx
+++ main.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Timer from './timer.jsx';
 
-ReactDOM.render(<Timer message="소집해제까지" target={Date.parse('2016-05-25 18:00:00+0900')} />, document.getElementById('wrapper'));
+ReactDOM.render(<Timer message="소집해제까지" target={1464166800000} />, document.getElementById('wrapper'));
