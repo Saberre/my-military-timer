422c2cb82079bcb73af9cf28b1ef3f582baf9320
Ki-baek Lee <saberre@gmail.com>
2016-04-06 13:39:31 +0900

    Meaningless refactoring

--- timer.jsx
+++ timer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styles from './timer.less';
 
-export default class extends React.Component {
+function Message(props) {
+  return (<div>
+    {props.message}
+  </div>);
+}
+
+class Counter extends React.Component {
   constructor() {
     super();
     this.state = {time: Date.now()};
@@ -13,12 +19,19 @@ export default class extends React.Component {
     setInterval(this._update.bind(this), 1000);
   }
   render() {
-    const { props: { target, message } } = this;
+    const { props: { target }} = this;
     let diff = Math.floor((target - this.state.time) / 1000);
     let secs = diff % 60;
     let mins = Math.floor(diff / 60 % 60);
     let hours = Math.floor(diff / 60 / 60 % 24);
     let days = Math.floor(diff / 60 / 60 / 24);
-    return <div className={styles.wrapper}><div className={styles.timer}>{message}<br />{days}일 {hours}시간 {mins}분 {secs}초</div></div>;
+    return (<div>{days}일 {hours}시간 {mins}분 {secs}초</div>);
   }
+}
+
+export default function (props) {
+  return (<div className={styles.wrapper}><div className={styles.timer}>
+    <Message message={props.message} />
+    <Counter target={props.target} />
+  </div></div>);
 }
