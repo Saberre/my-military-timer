9fb1681c5d8cfdbc570dfcbaa353c22d03ccd359
Ki-baek Lee <saberre@gmail.com>
2016-04-19 16:18:11 +0900

    Applied viewport-units-buggyfill

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Timer from './timer.jsx';
+import ViewportUnitsBuggyfill from 'viewport-units-buggyfill';
 
 import './main.less';
 
@@ -8,3 +9,5 @@ ReactDOM.render(
   <Timer message="소집해제까지" target={1464166800000} />,
   document.getElementById('app')
 );
+
+ViewportUnitsBuggyfill.init();
