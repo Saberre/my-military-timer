dc3157ec20294c2f2eabdfcb11afa10f57ea3512
Ki-baek Lee <saberre@gmail.com>
2016-04-19 15:33:11 +0900

    Extracted global tag selectors

--- main.jsx
+++ main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Timer from './timer.jsx';
 
+import './main.less';
+
 ReactDOM.render(
   <Timer message="소집해제까지" target={1464166800000} />,
   document.getElementById('app')
