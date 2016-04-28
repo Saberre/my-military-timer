50c3033e0129442dbc1f91dbd6159ff6f7062921
Ki-baek Lee <saberre@gmail.com>
2016-04-28 14:29:05 +0900

    I like to simplify things

--- timer.jsx
+++ timer.jsx
@@ -23,9 +23,9 @@ class Counter extends React.Component {
     const { props: { target }} = this;
     let diff = Math.floor((target - this.state.time) / 1000);
     let secs = diff % 60;
-    let mins = Math.floor(diff / 60 % 60);
-    let hours = Math.floor(diff / 60 / 60 % 24);
-    let days = Math.floor(diff / 60 / 60 / 24);
+    let mins = Math.floor((diff /= 60) % 60);
+    let hours = Math.floor((diff /= 60) % 24);
+    let days = Math.floor(diff / 24);
     return (<div>{days}일 {hours}시간 {mins}분 {secs}초</div>);
   }
 }
